c48cf247f8bcdaf35b9f5d661bf4fed202e2303b
Ejika Collins Ojochenemi <160156263+happyboy24@users.noreply.github.com>
2025-10-29 10:13:09 +0100

    Update day_three_test.py

--- day_three_test.py
+++ day_three_test.py
@@ -3,8 +3,8 @@ from day_three import *
 
 class TestSplit(unittest.TestCase):
 	def test_that_split_string_works(self):
-		actual = split_string("Eniife is really cool")
-		expected = ["Eniife", "is", "really", "cool"]
+		actual = split_string("Ebunoluwa is really cool")
+		expected = ["Ebunoluwa", "is", "really", "cool"]
 		self.assertEqual(actual,expected)
 
 	def test_that_split_string_only_takes_in_words(self):
